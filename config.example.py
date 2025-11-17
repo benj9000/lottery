530e4c86a1bde928666f7d6923842dd48613036b
benj9000 <49900294+benj9000@users.noreply.github.com>
2025-11-18 00:08:37 +0100

    Add example config.py

---
+++ config.example.py
@@ -0,0 +1,8 @@
+ticket_number: str = "1234567"
+picks: list[tuple[int, int, int, int, int, int]] = [
+    (1, 2, 3, 4, 5, 6),
+    (11, 12, 13, 14, 15, 16),
+    (21, 22, 23, 24, 25, 26),
+]
+play_spiel77: bool = True
+play_super6: bool = True
